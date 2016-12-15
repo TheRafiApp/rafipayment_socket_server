4915ffe12c6d5013a7d4dfae21bd73829bea0a55
Nick Ford <nickforddesign@gmail.com>
2016-12-14 19:58:55 -0500

    set up to use mailchimp api

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const fs = require('fs');
-const sqlite = require('sql.js');
-
-const filebuffer = fs.readFileSync('db/usda-nnd.sqlite3');
-
-const db = new sqlite.Database(filebuffer);
+const cors = require('cors');
+const mcapi = require('mailchimp-api');
+const bodyParser = require('body-parser')
 
 const app = express();
 
@@ -15,54 +13,26 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
-const COLUMNS = [
-  'carbohydrate_g',
-  'protein_g',
-  'fa_sat_g',
-  'fa_mono_g',
-  'fa_poly_g',
-  'kcal',
-  'description',
-];
-app.get('/api/food', (req, res) => {
-  const param = req.query.q;
+app.use(cors())
+app.use(bodyParser.json())
 
-  if (!param) {
-    res.json({
-      error: 'Missing required parameter `q`',
+const mc = new mcapi.Mailchimp('ed3d1c577fb1a130d7e83b0df443b9c0-us11');
+
+
+app.post('/api/request', (req, res) => {
+
+  mc.lists.subscribe({id: 'b8bb9fbd61', email:{email:req.body.email}}, function(data) {
+      console.log(data)
+      res.json({ message: 'User subscribed successfully! Look for the confirmation email.' });
+
+    },
+    function(error) {
+      if (error.error) {
+        res.json({ error: error.error})
+      } else {
+        res.json({ error: 'There was an error subscribing that user' });
+      }
     });
-    return;
-  }
-
-  // WARNING: Not for production use! The following statement
-  // is not protected against SQL injections.
-  const r = db.exec(`
-    select ${COLUMNS.join(', ')} from entries
-    where description like '%${param}%'
-    limit 100
-  `);
-
-  if (r[0]) {
-    res.json(
-      r[0].values.map((entry) => {
-        const e = {};
-        COLUMNS.forEach((c, idx) => {
-          // combine fat columns
-          if (c.match(/^fa_/)) {
-            e.fat_g = e.fat_g || 0.0;
-            e.fat_g = (
-              parseFloat(e.fat_g, 10) + parseFloat(entry[idx], 10)
-            ).toFixed(2);
-          } else {
-            e[c] = entry[idx];
-          }
-        });
-        return e;
-      })
-    );
-  } else {
-    res.json([]);
-  }
 });
 
 app.listen(app.get('port'), () => {
