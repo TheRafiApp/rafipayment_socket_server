a127b34532ccb0c5367ef2692c0e720bf196fe55
Nick Ford <nickforddesign@gmail.com>
2016-12-28 10:54:53 -0500

    removed env static assets config

--- server.js
+++ server.js
@@ -9,17 +9,16 @@ const app = express();
 app.set('port', (process.env.PORT || 3001));
 
 // Express only serves static assets in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
+// }
 
 app.use(cors())
 app.use(bodyParser.json())
 
 const mc = new mcapi.Mailchimp('ed3d1c577fb1a130d7e83b0df443b9c0-us11');
 
-
-app.post('/api/request', (req, res) => {
+app.post('/', (req, res) => {
 
   mc.lists.subscribe({id: 'b8bb9fbd61', email:{email:req.body.email}}, function(data) {
       console.log(data)
